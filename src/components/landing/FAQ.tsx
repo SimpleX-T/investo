d035f57bdd1eac9b190b3e6a6d4bc8a4414cf04c
SimpleX-T <ntmark2004@gmail.com>
2025-04-17 19:38:09 +0100

    fixed issue with text

--- src/components/landing/FAQ.tsx
+++ src/components/landing/FAQ.tsx
@@ -51,7 +51,7 @@ export default function FAQSection() {
       <div>
         <div className="flex w-full max-w-7xl mx-auto items-end justify-between space-y-24 md:space-y-0 flex-col md:flex-row">
           <div className="w-full md:w-[50%]">
-            <div className="text-center w-full px-4 md:p-0 mb-6 md:text-left border">
+            <div className="text-center w-full px-4 md:p-0 mb-6 md:text-left">
               <h2 className="font-bold text-[32px] md:text-5xl mb-4 w-[15ch] mx-auto">
                 Smooth and Easy International Transfers.
               </h2>
