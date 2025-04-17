a217f17614c53fb49874b076c7035e427742054c
SimpleX-T <ntmark2004@gmail.com>
2025-04-17 15:19:09 +0100

    initial commit: started landing page (hero section done)

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "../components/landing/hero";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <HeroSection />
+      <section></section>
+    </main>
+  );
+}
